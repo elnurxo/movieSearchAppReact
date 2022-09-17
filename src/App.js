54f42d1d57bc47c0b1a30c2396b2520bdd83ea7e
elnurxo <elnurykh@code.edu.az>
2022-09-17 18:43:47 +0400

    https bug fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
-const API_URL = "http://www.omdbapi.com?apikey=c831554d";
+const API_URL = "https://www.omdbapi.com?apikey=c831554d";
 
 function App() {
   const [movies,setMovies] = useState([]);
